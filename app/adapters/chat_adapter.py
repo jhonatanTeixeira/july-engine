af3f1b6f1660454e379d111478427e1e3cba9668
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2026-05-18 11:04:28 -0300

    more july_agent fixes

--- app/adapters/chat_adapter.py
+++ app/adapters/chat_adapter.py
@@ -75,6 +75,8 @@ class ChatAdapter(AdapterBase):
 
         helper = MultiModalHelper(payload)
 
+        print(payload)
+
         if not self.meta.get("is_vision"):
             await helper.process_vision()
         else:
