23838fbb87b364afd8f4f483594b0572afcf510b
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2026-06-14 09:55:23 -0300

    mais testes de integração e observabilidade

--- main.py
+++ main.py
@@ -273,4 +273,4 @@ async def prometheus_metrics():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
